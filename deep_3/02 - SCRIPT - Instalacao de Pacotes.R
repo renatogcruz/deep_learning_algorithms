6970e6b4ff411156b7fddca1e1524dde3610fcdb
renatogcruz <renatogcruz@hotmail.com>
2023-08-01 11:59:27 -0300

    aula 04 de deep learning revisão

--- deep_3/02 - SCRIPT - Instalacao de Pacotes.R
+++ deep_3/02 - SCRIPT - Instalacao de Pacotes.R
@@ -2,7 +2,9 @@
 #                  INSTALAÇÃO E CARREGAMENTO DE PACOTES NECESSÁRIOS             #
 ##################################################################################
 #Pacotes utilizados
-pacotes <- c("devtools","ggplot2","dplyr")
+pacotes <- c("devtools",
+             "ggplot2",
+             "dplyr")
 
 if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
   instalador <- pacotes[!pacotes %in% installed.packages()]
